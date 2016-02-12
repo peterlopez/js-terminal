7d44cb9732be8816fd43f7f6de11830cea6b9150
Peter Lopez <peter@peterlopezweb.com>
2016-02-11 21:20:52 -0800

    fixed bug and revised documentation in Terminal prototype

--- src/Terminal.js
+++ src/Terminal.js
@@ -1,13 +1,13 @@
 /**
  * Initializes a new terminal object with options
  * @constructor
- * @param $display (jQuery object) Element which displays result of a command
- * @param $input (jQuery object) Element which recieves input via keyboard
+ * @param $display (jQuery object) element which displays result of a command
+ * @param $input (jQuery object) element which recieves input via keyboard
  */
 var Terminal = function($display, $input) {
    this.$display = $display;
    this.$input = $input;
-}
+};
 
 Terminal.prototype = Object.create(Terminal.prototype);
 Terminal.prototype.constructor = Terminal;
